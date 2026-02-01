629fc266e74ef9b4dec4966a6d210e4d31da6dca
Aman PS <amanps282005@gmail.com>
2026-02-01 22:00:57 +0530

    new update

--- frontend/src/components/ui/DropDown.jsx
+++ frontend/src/components/ui/DropDown.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import { useRef, useEffect, useState } from "react"
 const DropDown = () => {
+  const {isOpen, setIsOpen}=useState(false)
+  const dropDownRef= useRef(null)
+
+  useEffect(()=>{
+    const handleClickOutside=(event)=>{
+      if(dropDownRef.current && !dropDownRef.current.contains(event.target)){
+        setIsOpen(false)
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return ()=>{
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
   return (
-    <div>DropDown</div>
+    <div className="relative inline-block text-left" ref={dropDownRef}>
+      <div onClick={()=>setIsOpen(!isOpen)}>{trigger}</div>
+      {isOpen && (
+        <div className="absolute right-0 z-20 mt-2 w-30 original-top-right rounded-lg bg-white shadow-lg border border-slate-200 focus:outline-none" role='menu' aria-orientation='vertical' aria-labelledby='menu-button'tabIndex={-1}>
+          <div className="py-1 " role="none"> {children}</div>
+        </div>
+      )}
+    </div>
   )
 }
-
+export const DropDownItem=({onClick, children})=>{
+  return(
+    <button onClick={onClick} role="menuitem" className="flex items-center w-full px-4 py-2 text-sm text-slate-700 hover:bg-slate-700 text-left" tabIndex={-1}>{children}</button>
+  )
+}
 export default DropDown
--- frontend/src/pages/EditorPage.jsx
+++ frontend/src/pages/EditorPage.jsx
@@ -1,6 +1,64 @@
-import React from 'react'
+import {useEffect, useState, useRef, use} from 'react'
+import {useParams, useNavigate} from 'react-router-dom'
+import toast from 'react-hot-toast'
+import {
+  Sparkles,
+  FileDown,
+  Save,
+  Menu,
+  X,
+  Edit,
+  NoteBookText,
+  ChevronDown,
+  FileText
+} from 'lucide-react'
+
+import {arrayMove} from '@dnd-kit/sortable'
+import axiosInstance from '../utils/axiosInstance'
+import {API_PATHS} from '../utils/apiPaths'
+import Dropdown, {DropdownItem} from '../components/ui/DropDown'
+import InputField from '../components/ui/InputField'
+import Button from '../components/ui/Button'
+import Modal from '../components/ui/Modal'
+import SelectField from '../components/ui/SelectField'
 
 const EditorPage = () => {
+  const {bookId} = useParams()
+  const navigate= useNavigate()
+  const [book, setBook] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [isSaving, setIsSaving] = useState(false)
+  const [isUploading, setIsUploading] = useState(false)
+  const [selectedChapterIndex, setSelectedChapterIndex] = useState(0)
+  const [activeTab, setActiveTab] = useState('editor')
+  const fileInputRef= useRef(null) 
+  const [isSedwbarOpen, setIsSidebarOpen]= useState(false)
+  const [isOutlineModalOpen, setIsOutlineModalOpen]= useState(false)
+  const [aiTopic, setAiTopic]= useState('')
+  const [aiStyle, setAiStyle]= useState('Informative')
+  const [isGenerating, setIsGenerating]= useState(false)
+
+  useEffect(() => {
+    const feetchBook = async () =>{
+      try {
+        const response = await axiosInstance.get(
+          `${API_PATHS.BOOKS.GET_BOOK_BY_ID}/${bookId}`
+        )
+        setBook(response.data);
+      } catch (error) {
+        toast.error("Failed to fetch book data.")
+        navigate('/dashboard')
+      } finally {
+        setIsLoading(false)
+      }
+    };
+    feetchBook();
+  }, [bookId, navigate]);
+
+  const handelBookChange = (e) =>{
+    const {name, value} = e.target
+    setBook((prev)=>({...prev, [name]: value}))
+  }
   return (
     <div>EditorPage</div>
   )
